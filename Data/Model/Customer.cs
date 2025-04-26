806cffbefdbc53300278e5775ac5fd5fe0987860
georgiivanov152 <gogosiv@abv.bg>
2025-04-26 13:39:20 +0300

    Редакция по потребителския интерфейс

--- Data/Model/Customer.cs
+++ Data/Model/Customer.cs
@@ -3,11 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Data.Model
 {
+    /// <summary>
+    /// Клиент
+    /// </summary>
     public class Customer
     {
         [Key]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Име на клиента
+        /// </summary>
         public string Name { get; set; }
 
         public string Address { get; set; }
--- Data/TravelContext.cs
+++ Data/TravelContext.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 
 namespace Data
 {
+    /// <summary>
+    /// Контекст на базата данни
+    /// </summary>
     public class TravelContext : DbContext
     {
+        /// <summary>
+        /// Низ за връзка към базата данни
+        /// </summary>
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WeTravel;Integrated Security=True";
 
         public DbSet<Model.Customer> Customers { get; set; }
